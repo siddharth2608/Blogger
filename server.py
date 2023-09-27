84b01d5b9f270795008937e54166c4455a9acba2
siddharth2608 <siddharthsrivastav987@gmail.com>
2023-09-27 11:41:57 +0530

    new

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("FLASK_BLOGGER_PORT", 5000))
+    app.run(debug=True,port=port)
 
